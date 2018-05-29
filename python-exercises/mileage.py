7df64740620173c17d030b07f3562cf2c0742b8c
Muhammad <muhammadraza0047@gmail.com>
2018-05-29 08:14:53 -0400

    Added a new file and nothing new in particular

---
+++ python-exercises/mileage.py
@@ -0,0 +1,4 @@
+#This is simple excercise part of the course. In this exercise I will build a conversion calculator
+user_input = input("How many km did you run today ")
+miles = int(user_input)/1.6
+print(f"Today you ran {miles} miles. Good Job")
